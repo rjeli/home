05381b3c2f20bc50be4ac8ac0570dfe30d2b3db1
Elijah Riggs <eriggs@polygon-n002hcy2c5.lan>
2024-10-12 15:45:51 -0400

    asdf

--- flake.nix
+++ flake.nix
@@ -18,23 +18,22 @@
     darwinConfig = { pkgs, ... }: {
       nixpkgs.hostPlatform = "aarch64-darwin";
 
+      system.configurationRevision = self.rev or self.dirtyRev or null;
       # backcompat: read `darwin-rebuild changelog` before changing
       system.stateVersion = 5;
 
       services.nix-daemon.enable = true;
       nix.package = pkgs.nix;
       nix.settings.experimental-features = "nix-command flakes";
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      users.users.eriggs = {
+        name = "eriggs";
+        home = "/Users/eriggs";
+      };
 
       programs.zsh.enable = true;
 
-      environment.systemPackages = [
-        pkgs.deno
-        pkgs.dhall
-        pkgs.dhall-json
-        pkgs.neofetch
-        pkgs.uv
-      ];
+      environment.systemPackages = with pkgs; [];
 
       system.defaults = {
         dock.autohide = true;
@@ -65,9 +64,63 @@
     };
     homeConfig = { pkgs, ... }: {
       home.stateVersion = "24.05";
-      # programs.home-manager.enable = true;
-      home.packages = with pkgs; [];
-      # home.sessionVariables = {EDITOR = "vim"; };
+
+      home.sessionVariables = {
+        EDITOR = "vim";
+      };
+      home.sessionPath = [
+        "$HOME/bin"
+        "$HOME/.cargo/bin"
+        "/opt/homebrew/bin"
+      ];
+
+      home.packages = with pkgs; [
+        deno
+        dhall
+        dhall-json
+        uv
+      ];
+
+      programs.zsh = {
+        enable = true;
+        defaultKeymap = "emacs";
+        history = {
+          # save timestamp
+          extended = true;
+          ignoreDups = true;
+          save = 999999999;
+          size = 999999999;
+        };
+        shellAliases = {
+          switch = "darwin-rebuild switch --flake ~/repos/home";
+          history = "history 0";
+          subl = "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl";
+        };
+        # added to .zshrc
+        initExtra = ''
+        setopt INC_APPEND_HISTORY
+
+        function parse_git_branch() {
+          git branch 2>/dev/null | sed -ne 's/^\* \(.*\)/[\1]/p'
+        }
+        setopt PROMPT_SUBST
+        export PROMPT='%n %F{green}%1~ %F{blue}$(parse_git_branch)%f %% '
+        '';
+      };
+
+      home.file.".vimrc".text = ''
+      inoremap jk <Esc>
+      '';
+
+      /*
+      programs.vim = {
+        enable = true;
+        defaultEditor = true;
+        extraConfig = ''
+        inoremap jk <Esc>
+        '';
+      };
+      */
     };
   in
   {
