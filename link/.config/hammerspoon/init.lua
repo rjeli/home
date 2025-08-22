2c8fbffb2d6b92c55ad759ccbc7fdc2693d549cf
rjeli <eli@rje.li>
2025-08-22 16:12:17 -0400

    hs: fix audio switch

--- link/.config/hammerspoon/init.lua
+++ link/.config/hammerspoon/init.lua
@@ -1,4 +1,52 @@
-hs.audiodevice.watcher.isRunning()
+print('init.lua')
 
-hs.notify.new({ title = "hamma", informativeText = "spoon" }):send()
-print('hi')
+-- hs.audiodevice.watcher.isRunning()
+--
+-- hs.notify.new({
+--     title = "hamma",
+--     informativeText = "spoon"
+-- }):send()
+--
+-- print('hi')
+--
+
+-- function getAllDeviceUids()
+--     local uids = {}
+--     for _, dev in ipairs(hs.audiodevice.allDevices()) do
+--         uids[dev:uid()] = true
+--     end
+--     return uids
+-- end
+
+-- local devs = {}
+
+local prevOutDev = hs.audiodevice.defaultOutputDevice()
+
+hs.audiodevice.watcher.setCallback(function(event)
+    -- 'dIn ', 'dOut': default in/out changed
+    -- 'sOut': system out (effects) changed
+    -- 'dev#': audio device added/removed
+
+    print('got event:', event)
+
+    inDev = hs.audiodevice.defaultInputDevice()
+    outDev = hs.audiodevice.defaultOutputDevice()
+
+    print('  inDev:', inDev)
+    print('  outDev:', outDev)
+
+    if outDev:uid() ~= prevOutDev:uid() then
+        if outDev:name() == "External Headphones" then
+            prevOutDev:setDefaultOutputDevice()
+            hs.alert(
+                'switched audio output back to ' .. prevOutDev:name(),
+                0.5
+            )
+        else
+            print('audio switched to ' .. outDev:name() .. ' (' .. outDev:uid() .. ')')
+            prevOutDev = outDev
+        end
+    end
+end)
+
+hs.audiodevice.watcher.start()
